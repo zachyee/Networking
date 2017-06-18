eee3d52776598be23853eef7c25519c08430d63f
Zachary Yee <Zachary.Yee@teradata.com>
2017-06-18 12:13:29 -0700

    Add echo client/server

---
+++ Echo_Networking/src/EchoClient.java
@@ -0,0 +1,48 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Socket;
+import java.net.UnknownHostException;
+
+/**
+ * Created by Zachary on 6/18/2017.
+ *
+ * https://docs.oracle.com/javase/tutorial/networking/sockets/examples/EchoClient.java
+ */
+public class EchoClient {
+    public static void printException(Exception e, String message) {
+        System.err.println(e.getStackTrace());
+        System.err.println(message);
+        System.exit(1);
+    }
+
+    public static void main(String[] args) {
+        if (args.length != 2) {
+            System.err.println("Usage: java EchoClient <host_name> <port_number>");
+            System.exit(1);
+        }
+
+        String hostName = args[0];
+        int portNumber = Integer.parseInt(args[1]);
+
+        try {
+            Socket echoSocket = new Socket(hostName, portNumber);
+            PrintWriter socketOut = new PrintWriter(echoSocket.getOutputStream(), true);
+            BufferedReader socketIn = new BufferedReader(new InputStreamReader(echoSocket.getInputStream()));
+            BufferedReader stdIn = new BufferedReader(new InputStreamReader(System.in));
+
+            String userInput;
+            while ((userInput = stdIn.readLine()) != null) {
+                socketOut.println(userInput);
+                System.out.println("echo: " + socketIn.readLine());
+            }
+        }
+        catch (UnknownHostException e) {
+            printException(e, "IP address of host (" + hostName + ") could not be determined.");
+        }
+        catch (IOException e) {
+            printException(e, "Could not create socket");
+        }
+    }
+}
---
+++ Echo_Networking/src/EchoServer.java
@@ -0,0 +1,39 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.net.UnknownHostException;
+
+/**
+ * Created by Zachary on 6/18/2017.
+ *
+ * https://docs.oracle.com/javase/tutorial/networking/sockets/examples/EchoServer.java
+ */
+public class EchoServer {
+    public static void main(String[]args) throws IOException {
+        if (args.length != 1) {
+            System.err.println("Usage: java EchoServer <port_number>");
+            System.exit(1);
+        }
+
+        int portNumber = Integer.parseInt(args[0]);
+
+        try {
+            ServerSocket serverSocket = new ServerSocket(portNumber);
+            Socket clientSocket = serverSocket.accept();
+            PrintWriter socketOut = new PrintWriter(clientSocket.getOutputStream(), true);
+            BufferedReader socketIn = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+
+            String inputLine;
+            while ((inputLine = socketIn.readLine()) != null) {
+                socketOut.println(inputLine);
+            }
+        }
+        catch (IOException e) {
+            System.err.println(e.getStackTrace());
+            System.err.println("Exception while listening to port " + portNumber);
+        }
+    }
+}
